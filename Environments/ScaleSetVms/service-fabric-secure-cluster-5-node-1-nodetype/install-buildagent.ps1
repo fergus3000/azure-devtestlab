b9986b05af277cf758ace831d93e2cf92a544f66
TommyJa <tommy.jakobsen@kdi.kongsberg.com>
2018-09-08 17:28:42 +0200

    getting .net core sdk installed

--- Environments/ScaleSetVms/service-fabric-secure-cluster-5-node-1-nodetype/install-buildagent.ps1
+++ Environments/ScaleSetVms/service-fabric-secure-cluster-5-node-1-nodetype/install-buildagent.ps1
@@ -22,11 +22,17 @@ Start-Transcript -Path 'c:\logs\install-buildagent-ps1-transcript.txt'
 $ipAddress = (Get-NetIPAddress | ? { $_.AddressFamily -eq "IPv4" -and ($_.IPAddress -match "10.0.0") }).IPAddress
 
 if (isNodeOne($ipAddress)) {
+
+    & ($PSScriptRoot + "\dotnet-install.ps1") -InstallDir c:/dotnet
+    $oldpath = (Get-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment' -Name PATH).path
+    $newpath = "$oldpath;c:\dotnet"
+    setx /M PATH $newpath
+
     $CurDir = get-location
     mkdir c:\agent
     Set-Location c:\agent
     Add-Type -AssemblyName System.IO.Compression.FileSystem
-    [System.IO.Compression.ZipFile]::ExtractToDirectory("$CurDir\vsts-agent-win-x64-2.139.1.zip", "$PWD")
+    [System.IO.Compression.ZipFile]::ExtractToDirectory("$CurDir\vsts-agent-win-x64-2.139.1.zip", "")
 
     .\config.cmd --unattended --url $ServerUrl --auth PAT --token $PersonalAccessToken --pool $PoolName --agent $AgentName --runasservice
 
