1a73e7182dd1d39f288c902e28b03044c8daeb10
TommyJa <tommy.jakobsen@kdi.kongsberg.com>
2018-09-10 17:31:54 +0200

    wait for dot net install to complete before installing build agent

--- Environments/ScaleSetVms/service-fabric-secure-cluster-5-node-1-nodetype/install-buildagent.ps1
+++ Environments/ScaleSetVms/service-fabric-secure-cluster-5-node-1-nodetype/install-buildagent.ps1
@@ -23,7 +23,9 @@ $ipAddress = (Get-NetIPAddress | ? { $_.AddressFamily -eq "IPv4" -and ($_.IPAddr
 
 if (isNodeOne($ipAddress)) {
 
-    ./dotnet-sdk-2.1.401-win-x64.exe /install /norestart /quiet /log "c:\logs\Dotnet Core SDK 2.1.105.log"
+#    ./dotnet-sdk-2.1.401-win-x64.exe /install /norestart /quiet /log "c:\logs\Dotnet Core SDK 2.1.105.log" 
+    Start-Process -FilePath "./dotnet-sdk-2.1.401-win-x64.exe" -ArgumentList "/install /norestart /quiet /log 'c:\logs\Dotnet Core SDK 2.1.105.log'" -PassThru -Wait
+
     #& ($PSScriptRoot + "\dotnet-install.ps1") -InstallDir c:/dotnet
     #$oldpath = (Get-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment' -Name PATH).path
     #$newpath = "$oldpath;c:\dotnet"
