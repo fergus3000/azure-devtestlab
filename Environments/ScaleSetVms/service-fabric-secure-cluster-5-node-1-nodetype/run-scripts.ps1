b95d11e80b5d2685b2ee77f1f17bb9ed64c4d117
TommyJa <tommy.jakobsen@kdi.kongsberg.com>
2018-09-10 17:34:35 +0200

    wait for build agent to complete install

--- Environments/ScaleSetVms/service-fabric-secure-cluster-5-node-1-nodetype/run-scripts.ps1
+++ Environments/ScaleSetVms/service-fabric-secure-cluster-5-node-1-nodetype/run-scripts.ps1
@@ -13,5 +13,6 @@ Param
     $AgentName
 )
 
-& ($PSScriptRoot + "\install-buildagent.ps1") -ServerUrl $ServerUrl -PersonalAccessToken $PersonalAccessToken -PoolName $PoolName -AgentName $AgentName
+#& ($PSScriptRoot + "\install-buildagent.ps1") -ServerUrl $ServerUrl -PersonalAccessToken $PersonalAccessToken -PoolName $PoolName -AgentName $AgentName
+Start-Process -FilePath "$PSScriptRoot\install-buildagent.ps1" -ArgumentList "-ServerUrl $ServerUrl -PersonalAccessToken $PersonalAccessToken -PoolName $PoolName -AgentName $AgentName" -PassThru -Wait
 & ($PSScriptRoot + "\install-couchbase.ps1")
