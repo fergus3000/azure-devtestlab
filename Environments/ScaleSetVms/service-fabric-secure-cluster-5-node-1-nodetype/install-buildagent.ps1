c70fddca3a8995230904437fa4046d9b53f7c8ee
TommyJa <tommy.jakobsen@kdi.kongsberg.com>
2018-09-10 10:41:00 +0200

    got the wrong .net core sdk

--- Environments/ScaleSetVms/service-fabric-secure-cluster-5-node-1-nodetype/install-buildagent.ps1
+++ Environments/ScaleSetVms/service-fabric-secure-cluster-5-node-1-nodetype/install-buildagent.ps1
@@ -23,10 +23,12 @@ $ipAddress = (Get-NetIPAddress | ? { $_.AddressFamily -eq "IPv4" -and ($_.IPAddr
 
 if (isNodeOne($ipAddress)) {
 
-    & ($PSScriptRoot + "\dotnet-install.ps1") -InstallDir c:/dotnet
-    $oldpath = (Get-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment' -Name PATH).path
-    $newpath = "$oldpath;c:\dotnet"
-    setx /M PATH $newpath
+    ./dotnet-sdk-2.1.401-win-x64.exe /install /norestart /quiet /log "c:\logs\Microsoft .NET Core SDK 2.1.105.log"
+
+    #& ($PSScriptRoot + "\dotnet-install.ps1") -InstallDir c:/dotnet
+    #$oldpath = (Get-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment' -Name PATH).path
+    #$newpath = "$oldpath;c:\dotnet"
+    #setx /M PATH $newpath
 
     $CurDir = get-location
     mkdir c:\agent
