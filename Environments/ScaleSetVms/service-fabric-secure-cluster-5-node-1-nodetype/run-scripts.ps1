b5754a7f58ae3b609a68e7cac2d7e1cb0a797f9d
TommyJa <tommy.jakobsen@kdi.kongsberg.com>
2018-09-11 08:44:03 +0200

    trying with dotnet first

--- Environments/ScaleSetVms/service-fabric-secure-cluster-5-node-1-nodetype/run-scripts.ps1
+++ Environments/ScaleSetVms/service-fabric-secure-cluster-5-node-1-nodetype/run-scripts.ps1
@@ -20,6 +20,19 @@ function isNodeOne($ipAddress) {
 
 Start-Transcript -Path 'c:\logs\run-scripts-ps1-transcript.txt'
 
+$ipAddress = (Get-NetIPAddress | ? { $_.AddressFamily -eq "IPv4" -and ($_.IPAddress -match "10.0.0") }).IPAddress
+if (isNodeOne($ipAddress)) {
+
+    Write-Host "Installing .net core sdk "
+    Get-Date
+
+    ./dotnet-sdk-2.1.401-win-x64.exe /install /quiet /log "c:\logs\Dotnet Core SDK 2.1.105.log"
+    #Start-Process -FilePath "./dotnet-sdk-2.1.401-win-x64.exe" -ArgumentList "/install /norestart /quiet /log 'c:\logs\Dotnet Core SDK 2.1.105.log'" -PassThru -Wait
+
+    Write-Host "Done installing .net core sdk "
+    Get-Date
+}
+
 Write-Host "running install-buildagent.ps1 "
 Get-Date
 
@@ -30,18 +43,6 @@ Get-Date
 
 & ($PSScriptRoot + "\install-couchbase.ps1")
 
-$ipAddress = (Get-NetIPAddress | ? { $_.AddressFamily -eq "IPv4" -and ($_.IPAddress -match "10.0.0") }).IPAddress
-if (isNodeOne($ipAddress)) {
-
-    Write-Host "Installing .net core sdk "
-    Get-Date
-
-    ./dotnet-sdk-2.1.401-win-x64.exe /install /norestart /quiet /log "c:\logs\Dotnet Core SDK 2.1.105.log"
-    #Start-Process -FilePath "./dotnet-sdk-2.1.401-win-x64.exe" -ArgumentList "/install /norestart /quiet /log 'c:\logs\Dotnet Core SDK 2.1.105.log'" -PassThru -Wait
-
-    Write-Host "Done installing .net core sdk "
-    Get-Date
-}
 Write-Host "done"
 
 Stop-Transcript
